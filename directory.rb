f8129acbe61c274e8bdebf45ea7716618567217c
James Gardiner <jamesgardiner075@gmail.com>
2016-09-17 11:52:19 +0100

    Add push to list method

--- directory.rb
+++ directory.rb
@@ -32,7 +32,7 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
+    push_to_list(name, cohort.to_sym)
   end
   file.close
 end
@@ -47,6 +47,10 @@ def try_load_students
     puts "Sorry, #{filename} doesn't exist."
     exit # quit the program
   end
+end
+
+def push_to_list (name, cohort = :november)
+  @students << {name: name, cohort: cohort}
 end
 
 def show_students
@@ -80,7 +84,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    push_to_list(name)
     puts "Now we have #{@students.count} students"
     # get another name from the user
     name = STDIN.gets.chomp
